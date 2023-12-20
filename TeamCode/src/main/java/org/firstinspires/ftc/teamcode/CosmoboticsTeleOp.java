f2071a6e7a93a8063e1a968639a628a1cb64f823
theo_wong <theodorejosephwong@gmail.com>
2023-12-20 10:39:40 -0600

    Created TeleOp, Updated Gradle Files, Created Robot File

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CosmoboticsTeleOp.java
@@ -0,0 +1,231 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import java.util.List;
+
+@TeleOp
+public class CosmoboticsTeleOp extends LinearOpMode {
+    Robot robot;
+    public double autoTurn(double x, double y){
+        //initialize robot target angle
+        double target = robot.botHeading;
+
+        //calculate target angle using joystick values
+        if (x != 0 || y != 0) {
+            target = Math.toDegrees(Math.atan2(y, -x));
+        }
+
+        //make target angle range 0 ~ 359.999...
+        if (target < 0){
+            target += 360;
+        }
+
+        double angleError = target - robot.botHeading;
+
+        if (angleError > 0){
+
+            //fine turning
+            if (Math.abs(angleError) < 8){
+                return 0.2;
+            }
+
+            return 0.7;
+
+        }else if (angleError < 0){
+            //fine turning
+            if (Math.abs(angleError) < 8){
+                return 0.2;
+            }
+
+            return -0.7;
+
+        } else {
+            return 0;
+        }
+
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new Robot(this);
+
+        ElapsedTime gamepadBackTime = new ElapsedTime();
+        ElapsedTime droneTime = new ElapsedTime();
+        ElapsedTime gamepadStartTime = new ElapsedTime();
+        ElapsedTime gamepadYTime = new ElapsedTime();
+        ElapsedTime gamepadATime = new ElapsedTime();
+        ElapsedTime gamepadBTime = new ElapsedTime();
+        ElapsedTime gamepad2leftBumperTime = new ElapsedTime();
+        ElapsedTime gamepad2rightBumperTime = new ElapsedTime();
+        ElapsedTime gamepad1leftBumperTime = new ElapsedTime();
+        ElapsedTime gamepad1rightBumperTime = new ElapsedTime();
+        ElapsedTime gamepadDpadRightTime = new ElapsedTime();
+        ElapsedTime gamepadDpadLeftTime = new ElapsedTime();
+
+        waitForStart();
+        if (isStopRequested()) return;
+        while (opModeIsActive()) {
+
+            //Field Centric Drive:
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+            //this version is robot centric
+            /*double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;*/
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers.
+            if (gamepad1.dpad_up) {
+                robot.resetImu();
+            }
+            //field centric ***/))
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-robot.botHeading) - y * Math.sin(-robot.botHeading);
+            double rotY = x * Math.sin(-robot.botHeading) + y * Math.cos(-robot.botHeading);
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (-rotY + rotX - rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+            robot.frontLeft.setPower(frontLeftPower);
+            robot.backLeft.setPower(backLeftPower);
+            robot.frontRight.setPower(frontRightPower);
+            robot.backRight.setPower(backRightPower);
+
+            //robot centric (there is different logic)
+            /*double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y + x - rx) / denominator;
+            double backRightPower = (y - x - rx) / denominator;
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);*/
+
+            //Game-Pad Booleans and Doubles:
+            boolean droneButton = gamepad2.back;
+            boolean intakeRotationButton = gamepad2.y;
+            boolean rightIntake = gamepad2.right_bumper;
+            boolean leftIntake = gamepad2.left_bumper;
+            boolean reset = gamepad1.a;
+            //Auto-Turn Buttons:
+            boolean dpadLeft = gamepad1.dpad_left;
+            boolean dpadRight = gamepad1.dpad_right;
+            boolean backButton = gamepad1.back;
+            boolean startButton = gamepad1.start;
+
+            //Drone Initialization:
+            if (droneButton && droneTime.milliseconds() > 500) {
+                robot.droneServo.setPosition(1);
+                droneTime.reset();
+            }
+
+            //Reset Arm Initialization:
+            if (reset && gamepadATime.milliseconds() > 500) {
+                robot.armTarget = 0;
+                robot.slidesTarget = 0;
+                robot.intakeRotation.setPosition(robot.intakeRotationPosStart);
+                robot.leftIntake.setPosition(0);
+                robot.rightIntake.setPosition(0);
+                gamepadATime.reset();
+            }
+
+            //Slides Initialization:
+            robot.slidesTarget += gamepad1.right_trigger * 25 - gamepad1.left_trigger * 15;
+
+            if (robot.slidesTarget < 0) {
+                robot.slidesTarget = 0;
+            } else if (robot.slidesTarget > 2500) {
+                robot.slidesTarget = 2500;
+            }
+
+            //Arm Initialization:
+            robot.armTarget += gamepad2.right_trigger * 25 - gamepad2.left_trigger * 15;
+
+            if (robot.armTarget < 0) {
+                robot.armTarget = 0;
+            } else if (robot.armTarget > 2500) {
+                robot.armTarget = 2500;
+            }
+
+            //Intake Rotation Initialization:
+            if (intakeRotationButton && gamepadYTime.milliseconds() > 500) {
+                if (robot.intakeRotationPos == robot.intakeRotationPosEnd) {
+                    robot.intakeRotation.setPosition(robot.intakeRotationPosStart);
+                } else {
+                    robot.intakeRotation.setPosition(robot.intakeRotationPosEnd);
+                }
+                gamepadYTime.reset();
+            }
+
+            //Claw Initializations:
+            if (rightIntake && gamepad1rightBumperTime.milliseconds() > 500) {
+                if (robot.rightIntakePos == 0) {
+                    robot.rightIntake.setPosition(1);
+                } else {
+                    robot.rightIntake.setPosition(0);
+                }
+                gamepad1rightBumperTime.reset();
+            }
+
+            if (leftIntake && gamepad1leftBumperTime.milliseconds() > 500) {
+                if (robot.leftIntakePos == 0) {
+                    robot.leftIntake.setPosition(1);
+                } else {
+                    robot.leftIntake.setPosition(0);
+                }
+                gamepad1leftBumperTime.reset();
+            }
+
+            //Auto-turn Initializations:
+            if (dpadLeft && gamepadDpadLeftTime.milliseconds() > 500) {
+                autoTurn(-.5, .5);
+                gamepadDpadLeftTime.reset();
+            }
+            if (dpadRight && gamepadDpadRightTime.milliseconds() > 500) {
+                autoTurn(.5, .5);
+                gamepadDpadRightTime.reset();
+            }
+            if (startButton && gamepadStartTime.milliseconds() > 500) {
+                autoTurn(1, 0);
+                gamepadStartTime.reset();
+            }
+            if (backButton && gamepadBackTime.milliseconds() > 500) {
+                autoTurn(-1, 0);
+                gamepadBackTime.reset();
+            }
+
+
+            //Telemetry:
+            telemetry.addData("ArmPos", robot.armPos);
+            telemetry.addData("ArmTarget", robot.armTarget);
+            telemetry.addData("SlidesPos", robot.slidesPos);
+            telemetry.addData("SlidesTarget", robot.slidesTarget);
+            telemetry.addData("IntakeRotationPos", robot.intakeRotationPos);
+            telemetry.addData("LeftIntakePos", robot.leftIntakePos);
+            telemetry.addData("RightIntakePos", robot.rightIntakePos);
+            telemetry.addData("FrontLeftPos", robot.frontLeftPos);
+            telemetry.addData("FrontRightPos", robot.frontRightPos);
+            telemetry.addData("BackLeftPos", robot.backLeftPos);
+            telemetry.addData("BackRightPos", robot.backRightPos);
+            telemetry.addData("Robot Heading", robot.botHeading);
+            telemetry.addData("Y", -gamepad1.left_stick_y);
+            telemetry.addData("X", gamepad1.left_stick_x);
+            telemetry.addData("RX", gamepad1.right_stick_x);
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,287 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.apache.commons.math3.util.FastMath.abs;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import android.graphics.Matrix;
+
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+import java.util.List;
+import java.util.RandomAccess;
+import java.util.Vector;
+
+
+public class Robot {
+    public DcMotorEx frontRight;
+    public DcMotorEx frontLeft;
+    public DcMotorEx backRight;
+    public DcMotorEx backLeft;
+    public DcMotorEx slidesMotor;
+
+    public DcMotorEx armMotor;
+
+    public ServoImplEx rightIntake;
+
+    public ServoImplEx leftIntake;
+    public ServoImplEx intakeRotation;
+    public ServoImplEx droneServo;
+    public PIDController arm_controller;
+    public static double arm_p = 0, arm_i = 0, arm_d = 0;
+    public static double arm_f = 0;
+    public static int armTarget = 0;
+    public final double arm_ticks_in_degrees = 1425.1;
+    public PIDController slides_controller;
+
+    public static double slides_p = 0, slides_i = 0, slides_d = 0;
+    public static double slides_f = 0;
+    public static int slidesTarget = 0;
+    public final double slides_ticks_in_degrees = 537.7;
+
+    //    public final double arm_max_acceleration = 0;
+//    public final double arm_max_velocity = 0;
+//
+//    public double arm_distance = 0;
+//    public ElapsedTime arm_elapsed_time = new ElapsedTime();
+//    public final double slides_max_acceleration = 0;
+//    public final double slides_max_velocity = 0;
+//    public double slides_distance = 0;
+//    public ElapsedTime slides_elapsed_time = new ElapsedTime();
+
+
+
+
+    IMU imu;
+
+    //Bulk Read:
+    public double intakeRotationPosStart = 0.31;
+    public double intakeRotationPosEnd = .92;
+    public double leftIntakePos = leftIntake.getPosition();
+    public double rightIntakePos = rightIntake.getPosition();
+    public double intakeRotationPos = intakeRotation.getPosition();
+    public int armPos = armMotor.getCurrentPosition();
+    public int slidesPos = slidesMotor.getCurrentPosition();
+    public int frontLeftPos = frontLeft.getCurrentPosition();
+    public int frontRightPos = frontRight.getCurrentPosition();
+    public int backLeftPos = backLeft.getCurrentPosition();
+    public int backRightPos = backRight.getCurrentPosition();
+
+    public double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+    //Source: CtrlAltFTC
+//    public double motion_profile(double max_acceleration, double max_velocity, double distance, ElapsedTime elapsed_time) {
+//        elapsed_time.reset(); //May be bad implementation :skull:
+//        // calculate the time it takes to accelerate to max velocity
+//        double acceleration_dt = max_velocity / max_acceleration;
+//
+//        // If we can't accelerate to max velocity in the given distance, we'll accelerate as much as possible
+//        double halfway_distance = distance / 2;
+//        double acceleration_distance = Math.pow(0.5 * max_acceleration * acceleration_dt, 2);
+//
+//        if (acceleration_distance > halfway_distance)
+//            acceleration_dt = Math.sqrt(halfway_distance / (0.5 * max_acceleration));
+//
+//        acceleration_distance = Math.pow(0.5 * max_acceleration * acceleration_dt, 2);
+//
+//        // recalculate max velocity based on the time we have to accelerate and decelerate
+//        max_velocity = max_acceleration * acceleration_dt;
+//
+//        // we decelerate at the same rate as we accelerate
+//        double deceleration_dt = acceleration_dt;
+//
+//        // calculate the time that we're at max velocity
+//        double cruise_distance = distance - 2 * acceleration_distance;
+//        double cruise_dt = cruise_distance / max_velocity;
+//        double deceleration_time = acceleration_dt + cruise_dt;
+//
+//        // check if we're still in the motion profile
+//        double entire_dt = acceleration_dt + cruise_dt + deceleration_dt;
+//        if (elapsed_time.milliseconds() > entire_dt)
+//            return distance;
+//
+//        // if we're accelerating
+//        if (elapsed_time.milliseconds() < acceleration_dt) {
+//            // use the kinematic equation for acceleration
+//            return Math.pow(0.5 * max_acceleration * elapsed_time.milliseconds(), 2);
+//        } else if (elapsed_time.milliseconds() < deceleration_time) {
+//            // if we're cruising
+//            acceleration_distance = Math.pow(0.5 * max_acceleration * acceleration_dt, 2);
+//            double cruise_current_dt = elapsed_time.milliseconds() - acceleration_dt;
+//
+//            // use the kinematic equation for constant velocity
+//            return acceleration_distance + max_velocity * cruise_current_dt;
+//        }
+//
+//        // if we're decelerating
+//        else {
+//            acceleration_distance = Math.pow(0.5 * max_acceleration * acceleration_dt, 2);
+//            cruise_distance = max_velocity * cruise_dt;
+//            deceleration_time = elapsed_time.milliseconds() - deceleration_time;
+//
+//            // use the kinematic equations to calculate the instantaneous desired position
+//            return Math.pow(acceleration_distance + cruise_distance + max_velocity * deceleration_time - 0.5 * max_acceleration * deceleration_time, 2);
+//        }
+//    }
+//   oh
+    public Robot(OpMode opmode){
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        frontRight = opmode.hardwareMap.get(DcMotorEx.class, "frontRight");
+        frontLeft = opmode.hardwareMap.get(DcMotorEx.class, "frontLeft");
+        backRight = opmode.hardwareMap.get(DcMotorEx.class, "backRight");
+        backLeft = opmode.hardwareMap.get(DcMotorEx.class, "backLeft");
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        armMotor = opmode.hardwareMap.get(DcMotorEx.class, "armMotor");
+        slidesMotor = opmode.hardwareMap.get(DcMotorEx.class, "slidesMotor");
+        armMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        slidesMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        //Arm PID + Motion Profiling:
+        arm_controller = new PIDController(arm_p, arm_i, arm_d);
+        arm_controller.setPID(arm_p, arm_i, arm_d);
+        double arm_pid = arm_controller.calculate(armPos, armTarget);
+        double arm_ff = Math.cos(Math.toRadians(armTarget / arm_ticks_in_degrees)) * arm_f;
+        double armPower = arm_pid + arm_ff;
+        armMotor.setPower(armPower);
+        //Slides PID + Motion Profiling:
+        slides_controller = new PIDController(slides_p, slides_i, slides_d);
+        slides_controller.setPID(slides_p, slides_i, slides_d);
+        double slides_pid = slides_controller.calculate(slidesPos, slidesTarget);
+        double slides_ff = Math.cos(Math.toRadians(slidesTarget / slides_ticks_in_degrees)) * slides_f;
+        double slidesPower = slides_pid + slides_ff;
+        armMotor.setPower(slidesPower);
+
+        intakeRotation = opmode.hardwareMap.get(ServoImplEx.class, "intakeRotation");
+        rightIntake = opmode.hardwareMap.get(ServoImplEx.class, "rightIntake");
+        leftIntake = opmode.hardwareMap.get(ServoImplEx.class, "leftIntake");
+        droneServo = opmode.hardwareMap.get(ServoImplEx.class, "droneLauncher");
+        intakeRotation.setDirection(ServoImplEx.Direction.FORWARD);
+        rightIntake.setDirection(ServoImplEx.Direction.FORWARD);
+        leftIntake.setDirection(ServoImplEx.Direction.REVERSE);
+        droneServo.setDirection(ServoImplEx.Direction.REVERSE);
+        leftIntake.setPosition(0);
+        rightIntake.setPosition(0);
+        intakeRotation.setPosition(intakeRotationPosStart);
+        droneServo.setPosition(0);
+
+        imu = opmode.hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+        imu.resetYaw();
+    }
+    public Robot(LinearOpMode linearOpMode){
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        frontRight = linearOpMode.hardwareMap.get(DcMotorEx.class, "frontRight");
+        frontLeft = linearOpMode.hardwareMap.get(DcMotorEx.class, "frontLeft");
+        backRight = linearOpMode.hardwareMap.get(DcMotorEx.class, "backRight");
+        backLeft = linearOpMode.hardwareMap.get(DcMotorEx.class, "backLeft");
+        frontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        armMotor = linearOpMode.hardwareMap.get(DcMotorEx.class, "armMotor");
+        slidesMotor = linearOpMode.hardwareMap.get(DcMotorEx.class, "slidesMotor");
+        armMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        slidesMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        armMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        slidesMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        //Arm PID:
+        arm_controller = new PIDController(arm_p, arm_i, arm_d);
+        arm_controller.setPID(arm_p, arm_i, arm_d);
+        double arm_pid = arm_controller.calculate(armPos, armTarget);
+        double arm_ff = Math.cos(Math.toRadians(armTarget / arm_ticks_in_degrees)) * arm_f;
+        double armPower = arm_pid + arm_ff;
+        armMotor.setPower(armPower);
+        //Slides PID:
+        slides_controller = new PIDController(slides_p, slides_i, slides_d);
+        slides_controller.setPID(slides_p, slides_i, slides_d);
+        double slides_pid = slides_controller.calculate(slidesPos, slidesTarget);
+        double slides_ff = Math.cos(Math.toRadians(slidesTarget / slides_ticks_in_degrees)) * slides_f;
+        double slidesPower = slides_pid + slides_ff;
+        armMotor.setPower(slidesPower);
+
+
+
+        intakeRotation = linearOpMode.hardwareMap.get(ServoImplEx.class, "intakeRotation");
+        rightIntake = linearOpMode.hardwareMap.get(ServoImplEx.class, "rightIntake");
+        leftIntake = linearOpMode.hardwareMap.get(ServoImplEx.class, "leftIntake");
+        droneServo = linearOpMode.hardwareMap.get(ServoImplEx.class, "droneLauncher");
+        intakeRotation.setDirection(ServoImplEx.Direction.FORWARD);
+        rightIntake.setDirection(ServoImplEx.Direction.FORWARD);
+        leftIntake.setDirection(ServoImplEx.Direction.REVERSE);
+        droneServo.setDirection(ServoImplEx.Direction.REVERSE);
+        leftIntake.setPosition(0);
+        rightIntake.setPosition(0);
+        intakeRotation.setPosition(.31);
+        droneServo.setPosition(0);
+
+        imu = linearOpMode.hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+        imu.resetYaw();
+    }
+
+    public IMU getImu(){
+        return imu;
+    }
+    public void resetImu(){
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+        imu.resetYaw();
+    }
+}
+
